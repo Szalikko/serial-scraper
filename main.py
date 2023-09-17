e80534e0bdb2d66410f2cdadee0d8b5863a15c3b
Szalikko <91461505+Szalikko@users.noreply.github.com>
2023-09-17 10:05:34 +0200

    get pc serial and model name

--- main.py
+++ main.py
@@ -2,3 +2,7 @@
 # create file for output
 output = open("output.txt", "w")
 
+# get pc serial number and model
+serial = subprocess.check_output('powershell.exe Get-WmiObject win32_bios | select Serialnumber').decode("utf-8") 
+model = subprocess.check_output('powershell.exe Get-CimInstance Win32_ComputerSystemProduct | Select Name').decode("utf-8")
+
